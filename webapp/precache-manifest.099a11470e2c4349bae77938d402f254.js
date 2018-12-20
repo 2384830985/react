7ddddc7ba4b3f7c99d2ca4a49f7dca625658d5d1
tang <jinxintang@iyuedian.com>
2018-12-20 14:54:34 +0800

    新建react 项目

---
+++ webapp/precache-manifest.099a11470e2c4349bae77938d402f254.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "7ecae7dc32f31e710fcb",
+    "url": "./static/js/main.7ecae7dc.chunk.js"
+  },
+  {
+    "revision": "44ef06a480e228bbee97",
+    "url": "./static/js/1.44ef06a4.chunk.js"
+  },
+  {
+    "revision": "7ecae7dc32f31e710fcb",
+    "url": "./static/css/main.66af94ed.chunk.css"
+  },
+  {
+    "revision": "44ef06a480e228bbee97",
+    "url": "./static/css/1.83c06056.chunk.css"
+  },
+  {
+    "revision": "4bed4401bfe3906c0fd1a07783c0b63e",
+    "url": "./index.html"
+  }
+];
